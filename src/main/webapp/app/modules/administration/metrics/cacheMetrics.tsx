a735aec7bbd953082c39c254de5ab782d81a22fb
jesshaw <jesshaw@126.com>
2024-09-03 15:23:48 +0800

    the  metrics initial version

---
+++ src/main/webapp/app/modules/administration/metrics/cacheMetrics.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { TextFormat, nanToZero } from 'react-jhipster';
+import { Table } from 'reactstrap';
+
+export interface ICacheMetricsProps {
+  cacheMetrics: any;
+  twoDigitAfterPointFormat: string;
+}
+
+export class CacheMetrics extends React.Component<ICacheMetricsProps> {
+  render() {
+    const { cacheMetrics, twoDigitAfterPointFormat } = this.props;
+    return (
+      <div>
+        <h3>Cache statistics</h3>
+        <Table>
+          <thead>
+            <tr>
+              <th>Cache Name</th>
+              <th>Cache Hits</th>
+              <th>Cache Misses</th>
+              <th>Cache Gets</th>
+              <th>Cache Hit %</th>
+              <th>Cache Miss %</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.keys(cacheMetrics).map(key => (
+              <tr key={key}>
+                <td>{key}</td>
+                <td>{cacheMetrics[key]['cache.gets.hit']}</td>
+                <td>{cacheMetrics[key]['cache.gets.miss']}</td>
+                <td>{cacheMetrics[key]['cache.gets.miss'] + cacheMetrics[key]['cache.gets.hit']}</td>
+                <td>
+                  <TextFormat
+                    value={nanToZero(
+                      (100 * cacheMetrics[key]['cache.gets.hit']) /
+                        (cacheMetrics[key]['cache.gets.hit'] + cacheMetrics[key]['cache.gets.miss']),
+                    )}
+                    type="number"
+                    format={twoDigitAfterPointFormat}
+                  />
+                </td>
+                <td>
+                  <TextFormat
+                    value={nanToZero(
+                      (100 * cacheMetrics[key]['cache.gets.miss']) /
+                        (cacheMetrics[key]['cache.gets.hit'] + cacheMetrics[key]['cache.gets.miss']),
+                    )}
+                    type="number"
+                    format={twoDigitAfterPointFormat}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
+}
--- src/main/webapp/app/modules/administration/metrics/metrics-bak.tsx
+++ src/main/webapp/app/modules/administration/metrics/metrics-bak.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-// import { Button, Col, Row } from 'reactstrap';
+import { Button, Col, Row } from 'reactstrap';
 import {
   CacheMetrics,
   DatasourceMetrics,
@@ -10,14 +10,12 @@ import {
   EndpointsRequestsMetrics,
   SystemMetrics,
   Translate,
-  translate,
 } from 'react-jhipster';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { APP_TIMESTAMP_FORMAT, APP_TWO_DIGITS_AFTER_POINT_NUMBER_FORMAT, APP_WHOLE_NUMBER_FORMAT } from 'app/config/constants';
 import { getSystemMetrics, getSystemThreadDump } from '../administration.reducer';
 import { useAppDispatch, useAppSelector } from 'app/config/store';
-import { Button } from 'primereact/button';
 
 export const MetricsPage = () => {
   const dispatch = useAppDispatch();
@@ -43,33 +41,38 @@ export const MetricsPage = () => {
         <Translate contentKey="metrics.title">Application Metrics</Translate>
       </h2>
       <p>
-        <Button
-          label={translate('health.refresh.button')}
-          icon={`pi ${isFetching ? 'pi-spin' : ''} pi-refresh`}
-          onClick={getMetrics}
-          disabled={isFetching}
-        />
+        <Button onClick={getMetrics} color={isFetching ? 'btn btn-danger' : 'btn btn-primary'} disabled={isFetching}>
+          <FontAwesomeIcon icon="sync" />
+          &nbsp;
+          <Translate component="span" contentKey="health.refresh.button">
+            Refresh
+          </Translate>
+        </Button>
       </p>
       <hr />
 
-      <div className="grid columns-1 md:columns-3">
-        <h3 className="md:col-span-3">
-          <Translate contentKey="metrics.jvm.title">JVM Metrics</Translate>
-        </h3>
-        <div>{metrics?.jvm ? <JvmMemory jvmMetrics={metrics.jvm} wholeNumberFormat={APP_WHOLE_NUMBER_FORMAT} /> : ''}</div>
-        <div>{threadDump ? <JvmThreads jvmThreads={threadDump} wholeNumberFormat={APP_WHOLE_NUMBER_FORMAT} /> : ''}</div>
-        <div>
-          {metrics?.processMetrics ? (
-            <SystemMetrics
-              systemMetrics={metrics.processMetrics}
-              wholeNumberFormat={APP_WHOLE_NUMBER_FORMAT}
-              timestampFormat={APP_TIMESTAMP_FORMAT}
-            />
-          ) : (
-            ''
-          )}
-        </div>
-      </div>
+      <Row>
+        <Col sm="12">
+          <h3>
+            <Translate contentKey="metrics.jvm.title">JVM Metrics</Translate>
+          </h3>
+          <Row>
+            <Col md="4">{metrics?.jvm ? <JvmMemory jvmMetrics={metrics.jvm} wholeNumberFormat={APP_WHOLE_NUMBER_FORMAT} /> : ''}</Col>
+            <Col md="4">{threadDump ? <JvmThreads jvmThreads={threadDump} wholeNumberFormat={APP_WHOLE_NUMBER_FORMAT} /> : ''}</Col>
+            <Col md="4">
+              {metrics?.processMetrics ? (
+                <SystemMetrics
+                  systemMetrics={metrics.processMetrics}
+                  wholeNumberFormat={APP_WHOLE_NUMBER_FORMAT}
+                  timestampFormat={APP_TIMESTAMP_FORMAT}
+                />
+              ) : (
+                ''
+              )}
+            </Col>
+          </Row>
+        </Col>
+      </Row>
 
       {metrics?.garbageCollector ? (
         <GarbageCollectorMetrics garbageCollectorMetrics={metrics.garbageCollector} wholeNumberFormat={APP_WHOLE_NUMBER_FORMAT} />
@@ -91,7 +94,7 @@ export const MetricsPage = () => {
         ''
       )}
 
-      {/* {metrics?.cache ? (
+      {metrics?.cache ? (
         <Row>
           <Col sm="12">
             <CacheMetrics cacheMetrics={metrics.cache} twoDigitAfterPointFormat={APP_TWO_DIGITS_AFTER_POINT_NUMBER_FORMAT} />
@@ -99,9 +102,9 @@ export const MetricsPage = () => {
         </Row>
       ) : (
         ''
-      )} */}
+      )}
 
-      {/* {metrics?.databases && JSON.stringify(metrics.databases) !== '{}' ? (
+      {metrics?.databases && JSON.stringify(metrics.databases) !== '{}' ? (
         <Row>
           <Col sm="12">
             <DatasourceMetrics datasourceMetrics={metrics.databases} twoDigitAfterPointFormat={APP_TWO_DIGITS_AFTER_POINT_NUMBER_FORMAT} />
@@ -109,7 +112,7 @@ export const MetricsPage = () => {
         </Row>
       ) : (
         ''
-      )} */}
+      )}
     </div>
   );
 };
