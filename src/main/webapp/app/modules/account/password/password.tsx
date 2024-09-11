9673a76c9ae99170345ecb6abadc33654b634139
jesshaw <jesshaw@126.com>
2024-09-11 21:29:22 +0800

    Generate the password and settings changed

--- src/main/webapp/app/modules/account/password/password.tsx
+++ src/main/webapp/app/modules/account/password/password.tsx
@@ -1,34 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import { Translate, translate, ValidatedField, ValidatedForm } from 'react-jhipster';
-import { Row, Col, Button } from 'reactstrap';
+import { Translate, translate, TranslatorContext } from 'react-jhipster';
+import { Controller, useForm } from 'react-hook-form';
+import { classNames } from 'primereact/utils';
+import { Password } from 'primereact/password';
+import { Button } from 'primereact/button';
 import { toast } from 'app/shared/layout/theme/toast-manager';
 
 import { useAppDispatch, useAppSelector } from 'app/config/store';
 import { getSession } from 'app/shared/reducers/authentication';
-import PasswordStrengthBar from 'app/shared/layout/password/password-strength-bar';
-import { savePassword, reset } from './password.reducer';
+import { savePassword, reset as resetPassword } from './password.reducer';
 
 export const PasswordPage = () => {
   const [password, setPassword] = useState('');
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(reset());
+    dispatch(resetPassword());
     dispatch(getSession());
     return () => {
-      dispatch(reset());
+      dispatch(resetPassword());
     };
   }, []);
-
-  const handleValidSubmit = ({ currentPassword, newPassword }) => {
-    dispatch(savePassword({ currentPassword, newPassword }));
-  };
 
   const updatePassword = event => setPassword(event.target.value);
 
   const account = useAppSelector(state => state.authentication.account);
   const successMessage = useAppSelector(state => state.password.successMessage);
   const errorMessage = useAppSelector(state => state.password.errorMessage);
+  const updateSuccess = useAppSelector(state => state.password.updateSuccess);
 
   useEffect(() => {
     if (successMessage) {
@@ -36,62 +35,146 @@ export const PasswordPage = () => {
     } else if (errorMessage) {
       toast.error(translate(errorMessage));
     }
-    dispatch(reset());
+    dispatch(resetPassword());
   }, [successMessage, errorMessage]);
 
+  const defaultValues = { currentPassword: null, newPassword: null, confirmPassword: null };
+
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+    reset,
+    watch,
+    trigger,
+  } = useForm({ defaultValues: defaultValues });
+
+  const newPassword = watch('newPassword', '');
+
+  useEffect(() => {
+    reset(defaultValues);
+  }, [reset, TranslatorContext.context.locale, updateSuccess]);
+
+  const onSubmit = ({ currentPassword, newPassword }) => {
+    dispatch(savePassword({ currentPassword, newPassword }));
+  };
+
+  const getFormErrorMessage = name => {
+    return errors[name] && <small className="p-error">{errors[name].message}</small>;
+  };
+
   return (
-    <div>
-      <Row className="justify-content-center">
-        <Col md="8">
-          <h2 id="password-title">
-            <Translate contentKey="password.title" interpolate={{ username: account.login }}>
-              Password for {account.login}
-            </Translate>
-          </h2>
-          <ValidatedForm id="password-form" onSubmit={handleValidSubmit}>
-            <ValidatedField
-              name="currentPassword"
-              label={translate('global.form.currentpassword.label')}
-              placeholder={translate('global.form.currentpassword.placeholder')}
-              type="password"
-              validate={{
-                required: { value: true, message: translate('global.messages.validate.newpassword.required') },
-              }}
-              data-cy="currentPassword"
-            />
-            <ValidatedField
-              name="newPassword"
-              label={translate('global.form.newpassword.label')}
-              placeholder={translate('global.form.newpassword.placeholder')}
-              type="password"
-              validate={{
-                required: { value: true, message: translate('global.messages.validate.newpassword.required') },
-                minLength: { value: 4, message: translate('global.messages.validate.newpassword.minlength') },
-                maxLength: { value: 50, message: translate('global.messages.validate.newpassword.maxlength') },
-              }}
-              onChange={updatePassword}
-              data-cy="newPassword"
-            />
-            <PasswordStrengthBar password={password} />
-            <ValidatedField
-              name="confirmPassword"
-              label={translate('global.form.confirmpassword.label')}
-              placeholder={translate('global.form.confirmpassword.placeholder')}
-              type="password"
-              validate={{
-                required: { value: true, message: translate('global.messages.validate.confirmpassword.required') },
-                minLength: { value: 4, message: translate('global.messages.validate.confirmpassword.minlength') },
-                maxLength: { value: 50, message: translate('global.messages.validate.confirmpassword.maxlength') },
-                validate: v => v === password || translate('global.messages.error.dontmatch'),
-              }}
-              data-cy="confirmPassword"
-            />
-            <Button color="success" type="submit" data-cy="submit">
-              <Translate contentKey="password.form.button">Save</Translate>
-            </Button>
-          </ValidatedForm>
-        </Col>
-      </Row>
+    <div className="l-card">
+      <h5>
+        <Translate contentKey="password.title" interpolate={{ username: account.login }}>
+          Password for {account.login}
+        </Translate>
+      </h5>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="l-form">
+          <div>
+            <label htmlFor="currentPassword">
+              <Translate contentKey="global.form.currentpassword.label" />
+            </label>
+            <div>
+              <Controller
+                control={control}
+                name="currentPassword"
+                rules={{
+                  required: { value: true, message: translate('global.messages.validate.newpassword.required') },
+                }}
+                render={({ field, fieldState }) => (
+                  <Password
+                    id={field.name}
+                    {...field}
+                    value={field.value ? field.value : ''}
+                    feedback={false}
+                    toggleMask
+                    onBlur={() => {
+                      trigger('currentPassword');
+                    }}
+                    className={classNames({
+                      'p-invalid': fieldState.invalid,
+                    })}
+                    placeholder={translate('global.form.currentpassword.placeholder')}
+                  />
+                )}
+              />
+              {getFormErrorMessage('currentPassword')}
+            </div>
+          </div>
+          <div>
+            <label htmlFor="newPassword">
+              <Translate contentKey="global.form.newpassword.label" />
+            </label>
+            <div>
+              <Controller
+                control={control}
+                name="newPassword"
+                rules={{
+                  required: { value: true, message: translate('global.messages.validate.newpassword.required') },
+                  minLength: { value: 4, message: translate('global.messages.validate.newpassword.minlength') },
+                  maxLength: { value: 50, message: translate('global.messages.validate.newpassword.maxlength') },
+                }}
+                render={({ field, fieldState }) => (
+                  <Password
+                    id={field.name}
+                    {...field}
+                    value={field.value ? field.value : ''}
+                    toggleMask
+                    onBlur={() => {
+                      trigger('newPassword');
+                    }}
+                    className={classNames({
+                      'p-invalid': fieldState.invalid,
+                    })}
+                    placeholder={translate('global.form.newpassword.placeholder')}
+                  />
+                )}
+              />
+              {getFormErrorMessage('newPassword')}
+            </div>
+          </div>
+          <div>
+            <label htmlFor="confirmPassword">
+              <Translate contentKey="global.form.confirmpassword.label" />
+            </label>
+            <div>
+              <Controller
+                control={control}
+                name="confirmPassword"
+                rules={{
+                  required: { value: true, message: translate('global.messages.validate.confirmpassword.required') },
+                  minLength: { value: 4, message: translate('global.messages.validate.confirmpassword.minlength') },
+                  maxLength: { value: 50, message: translate('global.messages.validate.confirmpassword.maxlength') },
+                  validate: v => v === newPassword || translate('global.messages.error.dontmatch'),
+                }}
+                render={({ field, fieldState }) => (
+                  <Password
+                    id={field.name}
+                    {...field}
+                    value={field.value ? field.value : ''}
+                    feedback={false}
+                    toggleMask
+                    onBlur={() => {
+                      trigger('confirmPassword');
+                    }}
+                    className={classNames({
+                      'p-invalid': fieldState.invalid,
+                    })}
+                    placeholder={translate('global.form.confirmpassword.placeholder')}
+                  />
+                )}
+              />
+              {getFormErrorMessage('confirmPassword')}
+            </div>
+          </div>
+        </div>
+
+        <div className="l-form-footer">
+          <Button type="submit" label={translate('password.form.button')} icon="pi pi-save" disabled={updateSuccess} />
+        </div>
+      </form>
     </div>
   );
 };
--- src/main/webapp/app/modules/account/settings/settings.tsx
+++ src/main/webapp/app/modules/account/settings/settings.tsx
@@ -1,22 +1,28 @@
 import React, { useEffect } from 'react';
-import { Button, Col, Row } from 'reactstrap';
-import { Translate, translate, ValidatedField, ValidatedForm, isEmail } from 'react-jhipster';
+import { InputText } from 'primereact/inputtext';
+import { classNames } from 'primereact/utils';
+import { Dropdown } from 'primereact/dropdown';
+import { Button } from 'primereact/button';
+import { Translate, translate, isEmail } from 'react-jhipster';
 import { toast } from 'app/shared/layout/theme/toast-manager';
 
-import { locales, languages } from 'app/config/translation';
+import { locales, languages, languageArray } from 'app/config/translation';
 import { useAppDispatch, useAppSelector } from 'app/config/store';
 import { getSession } from 'app/shared/reducers/authentication';
-import { saveAccountSettings, reset } from './settings.reducer';
+import { saveAccountSettings, reset as resetAccountSettings } from './settings.reducer';
+import { defaultValue } from 'app/shared/model/user.model';
+import { Controller, useForm } from 'react-hook-form';
 
 export const SettingsPage = () => {
   const dispatch = useAppDispatch();
   const account = useAppSelector(state => state.authentication.account);
   const successMessage = useAppSelector(state => state.settings.successMessage);
+  const updateSuccess = useAppSelector(state => state.settings.updateSuccess);
 
   useEffect(() => {
     dispatch(getSession());
     return () => {
-      dispatch(reset());
+      dispatch(resetAccountSettings());
     };
   }, []);
 
@@ -26,7 +32,24 @@ export const SettingsPage = () => {
     }
   }, [successMessage]);
 
-  const handleValidSubmit = values => {
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+    reset,
+    trigger,
+  } = useForm({
+    defaultValues: defaultValue,
+  });
+
+  useEffect(() => {
+    if (account) {
+      // aync data update the form
+      reset(account);
+    }
+  }, [account, reset]);
+
+  const onSubmit = values => {
     dispatch(
       saveAccountSettings({
         ...account,
@@ -35,66 +58,148 @@ export const SettingsPage = () => {
     );
   };
 
+  const getFormErrorMessage = name => {
+    return errors[name] && <small className="p-error">{errors[name].message}</small>;
+  };
+
   return (
-    <div>
-      <Row className="justify-content-center">
-        <Col md="8">
-          <h2 id="settings-title">
-            <Translate contentKey="settings.title" interpolate={{ username: account.login }}>
-              User settings for {account.login}
-            </Translate>
-          </h2>
-          <ValidatedForm id="settings-form" onSubmit={handleValidSubmit} defaultValues={account}>
-            <ValidatedField
-              name="firstName"
-              label={translate('settings.form.firstname')}
-              id="firstName"
-              placeholder={translate('settings.form.firstname.placeholder')}
-              validate={{
-                required: { value: true, message: translate('settings.messages.validate.firstname.required') },
-                minLength: { value: 1, message: translate('settings.messages.validate.firstname.minlength') },
-                maxLength: { value: 50, message: translate('settings.messages.validate.firstname.maxlength') },
-              }}
-              data-cy="firstname"
-            />
-            <ValidatedField
-              name="lastName"
-              label={translate('settings.form.lastname')}
-              id="lastName"
-              placeholder={translate('settings.form.lastname.placeholder')}
-              validate={{
-                required: { value: true, message: translate('settings.messages.validate.lastname.required') },
-                minLength: { value: 1, message: translate('settings.messages.validate.lastname.minlength') },
-                maxLength: { value: 50, message: translate('settings.messages.validate.lastname.maxlength') },
-              }}
-              data-cy="lastname"
-            />
-            <ValidatedField
-              name="email"
-              label={translate('global.form.email.label')}
-              placeholder={translate('global.form.email.placeholder')}
-              type="email"
-              validate={{
-                required: { value: true, message: translate('global.messages.validate.email.required') },
-                minLength: { value: 5, message: translate('global.messages.validate.email.minlength') },
-                maxLength: { value: 254, message: translate('global.messages.validate.email.maxlength') },
-                validate: v => isEmail(v) || translate('global.messages.validate.email.invalid'),
-              }}
-              data-cy="email"
-            />
-            <ValidatedField type="select" id="langKey" name="langKey" label={translate('settings.form.language')} data-cy="langKey">
-              {locales.map(locale => (
-                <option value={locale} key={locale}>
-                  {languages[locale].name}
-                </option>
-              ))}
-            </ValidatedField>
-            <Button color="primary" type="submit" data-cy="submit">
-              <Translate contentKey="settings.form.button">Save</Translate>
-            </Button>
-          </ValidatedForm>
-        </Col>
-      </Row>
+    <div className="l-card">
+      <h5>
+        <Translate contentKey="settings.title" interpolate={{ username: account.login }}>
+          User settings for {account.login}
+        </Translate>
+      </h5>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="l-form">
+          <div>
+            <label htmlFor="firstName">
+              <Translate contentKey="settings.form.firstname" />
+            </label>
+            <div>
+              <Controller
+                control={control}
+                name="firstName"
+                rules={{
+                  required: { value: true, message: translate('settings.messages.validate.firstname.required') },
+                  minLength: { value: 1, message: translate('settings.messages.validate.firstname.minlength') },
+                  maxLength: { value: 50, message: translate('settings.messages.validate.firstname.maxlength') },
+                }}
+                render={({ field, fieldState }) => (
+                  <InputText
+                    id={field.name}
+                    {...field}
+                    value={field.value ? field.value : ''}
+                    onBlur={() => {
+                      trigger('firstName');
+                    }}
+                    className={classNames({
+                      'p-invalid': fieldState.invalid,
+                    })}
+                    placeholder={translate('settings.form.firstname.placeholder')}
+                  />
+                )}
+              />
+              {getFormErrorMessage('firstName')}
+            </div>
+          </div>
+          <div>
+            <label htmlFor="lastName">
+              <Translate contentKey="settings.form.lastname" />
+            </label>
+            <div>
+              <Controller
+                control={control}
+                name="lastName"
+                rules={{
+                  required: { value: true, message: translate('settings.messages.validate.lastname.required') },
+                  minLength: { value: 1, message: translate('settings.messages.validate.lastname.minlength') },
+                  maxLength: { value: 50, message: translate('settings.messages.validate.lastname.maxlength') },
+                }}
+                render={({ field, fieldState }) => (
+                  <InputText
+                    id={field.name}
+                    {...field}
+                    value={field.value ? field.value : ''}
+                    onBlur={() => {
+                      trigger('lastName');
+                    }}
+                    className={classNames({
+                      'p-invalid': fieldState.invalid,
+                    })}
+                    placeholder={translate('settings.form.lastname.placeholder')}
+                  />
+                )}
+              />
+              {getFormErrorMessage('lastName')}
+            </div>
+          </div>
+          <div>
+            <label htmlFor="email">
+              <Translate contentKey="global.form.email.label" />
+            </label>
+            <div>
+              <Controller
+                control={control}
+                name="email"
+                rules={{
+                  required: { value: true, message: translate('global.messages.validate.email.required') },
+                  minLength: { value: 5, message: translate('global.messages.validate.email.minlength') },
+                  maxLength: { value: 254, message: translate('global.messages.validate.email.maxlength') },
+                  validate: v => isEmail(v) || translate('global.messages.validate.email.invalid'),
+                }}
+                render={({ field, fieldState }) => (
+                  <InputText
+                    id={field.name}
+                    {...field}
+                    value={field.value ? field.value : ''}
+                    onBlur={() => {
+                      trigger('email');
+                    }}
+                    className={classNames({
+                      'p-invalid': fieldState.invalid,
+                    })}
+                    placeholder={translate('global.form.email.placeholder')}
+                  />
+                )}
+              />
+              {getFormErrorMessage('email')}
+            </div>
+          </div>
+
+          <div>
+            <label htmlFor="langKey">
+              <Translate contentKey="settings.form.language" />
+            </label>
+            <div>
+              <Controller
+                control={control}
+                name="langKey"
+                render={({ field, fieldState }) => (
+                  <Dropdown
+                    id={field.name}
+                    value={field?.value}
+                    onChange={e => field.onChange(e.value)}
+                    options={languageArray}
+                    optionValue="key"
+                    optionLabel="value"
+                    showClear
+                    onBlur={() => {
+                      trigger('langKey');
+                    }}
+                    className={classNames({
+                      'p-invalid': fieldState.invalid,
+                    })}
+                  />
+                )}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="l-form-footer">
+          <Button type="submit" label={translate('settings.form.button')} icon="pi pi-save" disabled={updateSuccess} />
+        </div>
+      </form>
     </div>
   );
 };
